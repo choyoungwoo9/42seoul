94e83c80a31c699822f9b939eae4601d7e57df89
Youngwoo Cho <youngwch@c3r8s6.42seoul.kr>
2022-11-15 20:11:52 +0900

    a

--- libft/subject/ft_lstmap_bonus2.c
+++ libft/subject/ft_lstmap_bonus2.c
@@ -6,7 +6,7 @@
 /*   By: youngwch <youngwch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 20:01:57 by youngwch          #+#    #+#             */
-/*   Updated: 2022/11/15 19:49:34 by youngwch         ###   ########.fr       */
+/*   Updated: 2022/11/15 19:33:23 by youngwch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,37 +26,35 @@ static void	free_all_lst(t_list *start_lst, void (*del)(void *))
 	return ;
 }
 
-void	go(t_list **tmp_ptr, t_list **al_ptr, t_list **lst, void *(*f)(void *))
-{
-		(*tmp_ptr) = (*al_ptr);
-		(*tmp_ptr)->content = f((*lst)->content);
-		(*tmp_ptr)->next = (void *)(-1);
-		(*lst) = (*lst)->next;
-}
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*start_lst;
-	t_list	*tmp_ptr;
-	t_list	*al_ptr;
+	t_list	*tmp_lst;
+	t_list	*tmp2_lst;
 
 	if (lst == 0)
 		return (0);
 	start_lst = (t_list *)(malloc(sizeof(t_list)));
 	if (start_lst == 0)
 		return (0);
-	go(&tmp_ptr, &start_lst, &lst, f);
+	tmp_lst = start_lst;
+	start_lst -> content = f(lst -> content);
+	start_lst -> next = (void *)(-1);
+	lst = lst -> next;
 	while (lst)
 	{
-		al_ptr = (t_list *)(malloc(sizeof(t_list)));
-		if (al_ptr == (t_list *)('\0'))
+		tmp2_lst = (t_list *)(malloc(sizeof(t_list)));
+		if (tmp2_lst == (t_list *)('\0'))
 		{
 			free_all_lst(start_lst, del);
 			return (0);
 		}
-		tmp_ptr -> next = al_ptr;
-		go(&tmp_ptr, &al_ptr, &lst, f);
+		tmp_lst -> next = tmp2_lst;
+		tmp_lst = tmp2_lst;
+		tmp_lst -> content = f(lst -> content);
+		tmp_lst -> next = (void *)(-1);
+		lst = lst -> next;
 	}
-	tmp_ptr -> next = 0;
+	tmp_lst -> next = 0;
 	return (start_lst);
 }
