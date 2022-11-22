f8919ff1c8effc5d6aeb9c56a45f5dc61c1747aa
Youngwoo Cho <youngwch@c4r8s8.42seoul.kr>
2022-11-22 18:44:08 +0900

    a

---
+++ GetNextLine/subject/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youngwch <youngwch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 08:32:56 by youngwch          #+#    #+#             */
+/*   Updated: 2022/11/22 17:01:24 by youngwch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include<stdlib.h>
+# include<unistd.h>
+
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+	int				state_index;
+}	t_list;
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+#endif
