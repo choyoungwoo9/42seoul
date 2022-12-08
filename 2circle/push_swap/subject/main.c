ed5705a5bc1668a7921f5709ad8a545a0f87deee
조영우 <joyeong-u@joyeong-uui-MacBookAir.local>
2022-12-09 08:43:59 +0900

    d

--- 2circle/push_swap/subject/main.c
+++ 2circle/push_swap/subject/main.c
@@ -3,36 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: youngwch <youngwch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joyeong-u <joyeong-u@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 17:10:07 by youngwch          #+#    #+#             */
-/*   Updated: 2022/12/08 17:44:33 by youngwch         ###   ########.fr       */
+/*   Updated: 2022/12/08 23:01:52 by joyeong-u        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "stdio.h"
 
-void	push_top(t_queue **head, int number)
+void	push_top(t_stack **head, int number)
 {
-	t_queue	*new_queue;
+	t_stack	*new_stack;
 
-	new_queue = (t_queue *)malloc(sizeof(t_queue));
-	new_queue->number = number;
-	new_queue->next = *head;
-	*head = new_queue;
+	new_stack = (t_stack *)malloc(sizeof(t_stack));
+	new_stack->number = number;
+	new_stack->next = *head;
+	new_stack->prev = NULL;
+	*head = new_stack;
 }
+
+void	pust_bot(t_stack **tail, int number)
+{
+	t_stack *new_stack;
+	
+	new_stack = (t_stack *)malloc(sizeof(t_stack));
+	new_stack->number = number;
+	new_stack->next = NULL;
+	new_stack->prev = 
+}
+
 
 
 int main(int argc, char **argv)
 {
-	t_queue	*list_queue_a_head;
-	int num;
+	t_stack	*stack_head;
+	t_stack	*stack_head;
 
-	num = 5;
-	list_queue_a_head = NULL;
+	stack_head = NULL;
 
-	push_top(&list_queue_a_head, num);
-	push_bot(&list_queue_a_head);
-	pop_top(&list_queue_a_head);
-	pop_bot(&list_queue_a_head);
+	push_top(&stack_head, 5);
+	push_top(&stack_head, 4);
+	push_top(&stack_head, 3);
+	push_top(&stack_head, 2);
+	push_top(&stack_head, 1);
+
+	//1 -> 2 -> 3 -> 4 -> 5
+
+	push_bot(&stack_head, 10);
+	//pop_top(&stack_head);
+	//pop_bot(&stack_head);
+	while(stack_head)
+	{
+		printf("%d\n", stack_head->number);
+		stack_head = stack_head->next;
+	}
 }
--- 2circle/push_swap/subject/push_swap.h
+++ 2circle/push_swap/subject/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: youngwch <youngwch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joyeong-u <joyeong-u@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 14:59:54 by youngwch          #+#    #+#             */
-/*   Updated: 2022/12/08 17:46:13 by youngwch         ###   ########.fr       */
+/*   Updated: 2022/12/08 22:58:14 by joyeong-u        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-typedef	struct s_queue
+typedef	struct s_stack
 {
 	int	number;
-	struct s_queue	*next;
-	struct s_queue	*prev;
-}	t_queue;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}	t_stack;
 #endif
