69473a445432400b16625a4b9f64d19869ed9e86
Youngwoo Cho <youngwch@c6r7s5.42seoul.kr>
2024-02-16 18:04:00 +0900

    d

--- 5circle/Inception/subject/srcs/requirements/wordpress/start.sh
+++ 5circle/Inception/subject/srcs/requirements/wordpress/start.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [ ! -f "/var/www/html/wp-config.php" ]; then
-    cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-fi
+# rm -rf /var/www/html/* \
 
-sed -i "s/database_name_here/${MYSQL_DATABASE}/g" /var/www/html/wp-config.php
-sed -i "s/username_here/${MYSQL_USER}/g" /var/www/html/wp-config.php
-sed -i "s/password_here/${MYSQL_PASSWORD}/g" /var/www/html/wp-config.php
-sed -i "s/localhost/db/g" /var/www/html/wp-config.php
+# if [ ! -f "/var/www/html/wp-config.php" ]; then
+#     cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+# fi
+
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+
+cp -r /wordpress/* /var/www/html/ \
+&& cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php \
+&& sed -i "s/database_name_here/${MYSQL_DATABASE}/g" /var/www/html/wp-config.php \
+&& sed -i "s/username_here/${MYSQL_USER}/g" /var/www/html/wp-config.php \
+&& sed -i "s/password_here/${MYSQL_PASSWORD}/g" /var/www/html/wp-config.php \
+&& sed -i "s/localhost/db/g" /var/www/html/wp-config.php \
+
+fi
 
 mkdir -p /run/php
 
